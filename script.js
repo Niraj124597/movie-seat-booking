1d36f484407969000ca3ee68cb30cc1c930c9fef
Niraj Regmi <niraj.regmi005@gmail.com>
2021-01-24 13:11:38 -0800

    saving data to local storage

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ const movieSelect = document.getElementById('movie');
 // let ticketPrice = parseInt(movieLists.value);
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // This counts the number of seats selected and updates the total price
 function updateSelectCount() {
   // This creates a NodeList of selected seats
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeats = document.querySelectorAll('.row .seat.selected'); 
+  // This conversts the NodeList into an array
+  const selectedSeatsArr = [...selectedSeats];
+  // This gives an array of index of selected seat
+  const seatsIndex = selectedSeatsArr.map(seat => [...seats].indexOf(seat));
+  // It stores the seatsIndex to local storage
+  localStorage.setItem('selectedSeatsIndex', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -18,6 +30,7 @@ function updateSelectCount() {
 
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectCount();
 });
 
